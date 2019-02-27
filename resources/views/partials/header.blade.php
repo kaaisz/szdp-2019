ee9999fe7e06bf338ae5630dc98e86f64e9ed4d2
Kaai Suzuki <suzydp@me.com>
2019-02-26 18:45:42 -0800

    Modify header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,19 +13,20 @@
     </div>
     <!-- //"nav__list" will be a toggle class for open and close// -->
     <ul id="nav__list" class="nav__list">
-      <a class="nav__link" href="#works"><li>CASE STUDY</li></a>
+      <a class="nav__link" href="#works"><li>Case Study</li></a>
         <ul>
-          <a class="nav__link" href="zmagazine.html"><li>Z Magazine</li></a>
+          <!-- <a class="nav__link" href="zmagazine.html"><li>Z Magazine</li></a>
           <a class="nav__link" href="mylocal.html"><li>MyLocal</li></a>
           <a class="nav__link" href="greentable.html"><li>Green Table</li></a>
           <a class="nav__link" href="grabbbb.html"><li>Grabbbb</li></a>
           <a class="nav__link" href="shiori.html"><li>Shiori</li></a>
-          <a class="nav__link" href="tnr-acid.html"><li>Tenori Acid</li></a>
+          <a class="nav__link" href="tnr-acid.html"><li>Tenori Acid</li></a> -->
+          <a class="nav__link" href="category/personal-work"><li>Archive</li></a>
         </ul>
       </li>
-      <a class="nav__link" href="#bio"><li>BIOGRAPHY</li></a>
-      <a class="nav__link" href="#skills"><li>SKILL</li></a>
-      <a class="nav__link" href="#contact"><li>CONTACT</li></a>
+      <a class="nav__link" href="#bio"><li>Biography</li></a>
+      <a class="nav__link" href="#skills"><li>Skill</li></a>
+      <a class="nav__link" href="#contact"><li>Contact</li></a>
       <div class="language">
         <li>EN / <a class="lang" href="jp/index.html">JP</a></li>
       </div>
