4fd7e67f884b6d8f36f42e0de39d26ff4524f08e
Kaai Suzuki <suzydp@me.com>
2019-03-20 12:35:40 -0700

    Fix header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,22 +13,13 @@
     </div>
     <!-- //"nav__list" will be a toggle class for open and close// -->
     <ul id="nav__list" class="nav__list">
-      <a class="nav__link" href="#works"><li>Case Study</li></a>
-        <ul>
-          <!-- <a class="nav__link" href="zmagazine.html"><li>Z Magazine</li></a>
-          <a class="nav__link" href="mylocal.html"><li>MyLocal</li></a>
-          <a class="nav__link" href="greentable.html"><li>Green Table</li></a>
-          <a class="nav__link" href="grabbbb.html"><li>Grabbbb</li></a>
-          <a class="nav__link" href="shiori.html"><li>Shiori</li></a>
-          <a class="nav__link" href="tnr-acid.html"><li>Tenori Acid</li></a> -->
-          <a class="nav__link" href="{{ home_url('/') }}category/work"><li>Archive</li></a>
-        </ul>
-      </li>
-      <a class="nav__link" href="#bio"><li>Biography</li></a>
-      <a class="nav__link" href="#skills"><li>Skill</li></a>
+      <a class="nav__link" href="{{ home_url('/') }}/#works"><li>Case Study</li></a>
+      <a class="nav__link" href="{{ home_url('/') }}category/work"><li>Archive</li></a>
+      <a class="nav__link" href="{{ home_url('/') }}/#bio"><li>Biography</li></a>
+      <a class="nav__link" href="{{ home_url('/') }}/#skills"><li>Skills</li></a>
       <a class="nav__link" href="#contact"><li>Contact</li></a>
       <div class="language">
-        <li>EN / <a class="lang" href="jp/index.html">JP</a></li>
+        <!-- <li>EN / <a class="lang" href="jp/index.html">JP</a></li> -->
       </div>
     </ul>
   </nav>
