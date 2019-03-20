e716f5c59cc9b5a40377392aa3f5be78e3c3a90d
Kaai Suzuki <suzydp@me.com>
2019-03-20 01:03:16 -0700

    Disable Scrollmagic pins if userAgent was match with mobile

--- resources/assets/scripts/scroll-animation.js
+++ resources/assets/scripts/scroll-animation.js
@@ -3,34 +3,36 @@ import ScrollMagic from 'scrollmagic';
 import 'debug.addIndicators';
 
 (function($) {
-  $(function () {
-    //-//-//-//-//-//-//-//-//-//-//-//-//-//
-    //-//----------ScrollMagic----------//-//
-    //-//-//-//-//-//-//-//-//-//-//-//-//-//
-    // init scrollmagic
-    const controller = new ScrollMagic.Controller();
-    const trigger = document.querySelector('.bio__img_wrap');
-    const stickyStart = trigger.offsetTop;
-    const stickyEnd = stickyStart + (document.querySelector('#bio').offsetHeight - 680);
-    console.log(controller);
-    
-
-    // // multiple pin in a single element
-    new ScrollMagic.Scene({
-      // define options by creating projects
-      triggerElement: '.bio__img_wrap',
-      triggerHook: 0,
-      duration: stickyEnd,
+  if(!navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)){
+    $(function () {
+      //-//-//-//-//-//-//-//-//-//-//-//-//-//
+      //-//----------ScrollMagic----------//-//
+      //-//-//-//-//-//-//-//-//-//-//-//-//-//
+      // init scrollmagic
+      const controller = new ScrollMagic.Controller();
+      const trigger = document.querySelector('.bio__img_wrap');
+      const stickyStart = trigger.offsetTop;
+      const stickyEnd = stickyStart + (document.querySelector('#bio').offsetHeight - 680);
+      console.log(controller);
+      
+  
+      // // multiple pin in a single element
+      new ScrollMagic.Scene({
+        // define options by creating projects
+        triggerElement: '.bio__img_wrap',
+        triggerHook: 0,
+        duration: stickyEnd,
+      })
+      .setPin('.bio__img_wrap', {pushFollowers: false})
+      // .addIndicators({
+      //   name: 'fade scene',
+      //   colorTrigger: 'black',
+      //   indent: '0',
+      //   colorStart: '#75C695',
+      // })
+      .addTo(controller);
     })
-    .setPin('.bio__img_wrap', {pushFollowers: false})
-    // .addIndicators({
-    //   name: 'fade scene',
-    //   colorTrigger: 'black',
-    //   indent: '0',
-    //   colorStart: '#75C695',
-    // })
-    .addTo(controller);
-  })
+  }
 })(jQuery);
 
 module.exports = $;
