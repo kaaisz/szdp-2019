84cb11d09903990ddd74f717365d2b722d0ccf67
Kaai Suzuki <suzydp@me.com>
2019-05-28 18:16:25 -0700

    Apply ACF on single.blade.php

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
+<?php 
+  $article_eyecatch = get_field('article_eyecatch', '3216')
+?>
+
+<div class="article__overview">
+  <img class="article__eyecatch" src="<?php the_field('article_eyecatch'); ?>" />
+</div>
+
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
+  @while(have_posts()) 
+    @php 
+      the_post() 
+    @endphp
     @include('partials.content-single-'.get_post_type())
   @endwhile
 @endsection
